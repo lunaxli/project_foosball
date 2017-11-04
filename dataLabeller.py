58cd756edb58a6a3b9b66e35330a868fba545cdb
Luna <lunaxli@ucla.edu>
2017-11-03 17:44:58 -0700

    Detect rccReconfig messages

--- dataLabeller.py
+++ dataLabeller.py
@@ -8,6 +8,11 @@ from time import sleep
 from mobile_insight.monitor import OfflineReplayer
 from mobile_insight.analyzer import MsgLogger, LteRrcAnalyzer, WcdmaRrcAnalyzer, LteNasAnalyzer, UmtsNasAnalyzer, LtePhyAnalyzer, LteMacAnalyzer
 
+
+messages = ["lte-rrc.rrcConnectionReconfiguration_element",
+			"lte-rrc.rrcConnectionReconfigurationComplete_element",
+			"lte-rrc.rrcConnectionRelease_element"]
+
 def dump_logs(path):
 	logger = MsgLogger()
 	logger.set_decode_format(MsgLogger.XML)
@@ -19,7 +24,7 @@ def dump_logs(path):
 	lte_rrc_analyzer.set_source(src)
 
 	wcdma_rrc_analyzer = WcdmaRrcAnalyzer()
-	wcdma_rrc_analyzer.set_source(src)  # bind with the monitor
+	wcdma_rrc_analyzer.set_source(src)
 
 	lte_nas_analyzer = LteNasAnalyzer()
 	lte_nas_analyzer.set_source(src)
@@ -34,11 +39,26 @@ def dump_logs(path):
 
 def locate_handoff(root):
 	for pkt in root:
-		print pkt.tag, pkt.attrib
+		#print pkt.tag, pkt.attrib
 
 		for pair in pkt.iter('pair'):
-			print "    ", pair.attrib, pair.text
-
+			#print "    ", pair.attrib, pair.text
+			if pair.attrib['key'] == "Msg":
+				for elem in pair[0][0]:
+					#print "        ", elem.attrib
+					if elem.attrib["name"] == "fake-field-wrapper":
+						for a in elem[0].iter('field'):
+							if a.get("name") == "lte-rrc.message":
+								for b in a:
+									if b.get("name") == "lte-rrc.c1":
+										for c in b.iter("field"):
+											#print c.attrib
+											if c.attrib["name"] == messages[0]:
+												print "Reconfig Start"
+											if c.attrib["name"] == messages[1]:
+												print "Reconfig Complete"
+											if c.attrib["name"] == messages[2]:
+												print "Reconfig Release"
 
 if __name__ == "__main__":
 	src = OfflineReplayer()
